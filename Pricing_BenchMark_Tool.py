6823a906ea2aa9d44cd66259412194fcf2ddb0d3
Miguel Mñz <160363172+Mikel-Mnz@users.noreply.github.com>
2024-04-20 18:14:20 -0600

    Update Pricing_BenchMark_Tool.py
    It first extracts the html of the products on the website, then parses the html and extracts the url of all the products.
    
    At the end, for testing purposes, a print was added to see the links and the length of the list.

--- Pricing_BenchMark_Tool.py
+++ Pricing_BenchMark_Tool.py
@@ -1,8 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-baseurl1 = "https://www.cyberpuerta.mx/Computo-Hardware/Servidores/Servidores/"
+baseurl_1 = "https://www.cyberpuerta.mx/"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
 }
+
+# Inicializar una lista para almacenar todos los enlaces
+all_productlinks = []
+
+for x in range(1, 7):
+    r = requests.get(
+        f'https://www.cyberpuerta.mx/Computo-Hardware/Servidores/Servidores/{x}')
+    soup = BeautifulSoup(r.content, 'lxml')
+
+    productList = soup.find_all(
+        'li', class_='cell productData small-12 small-order-1')
+
+    productList_2 = soup.find_all(
+        'li', class_='cell productData small-12 small-order-3')
+
+    productlinks = []
+
+    for item in productList:
+        for link in item.find_all('a', href=True):
+            if link['href'].startswith('https://www.cyberpuerta.mx/Computo-Hardware/Servidores/Servidores/'):
+                productlinks.append(link['href'])
+
+    for item in productList_2:
+        for link in item.find_all('a', href=True):
+            if link['href'].startswith('https://www.cyberpuerta.mx/Computo-Hardware/Servidores/Servidores/'):
+                productlinks.append(link['href'])
+
+    # Agregar los enlaces de la página actual a la lista de todos los enlaces
+    all_productlinks.extend(productlinks)
+
+# Eliminar duplicados
+all_productlinks = list(set(all_productlinks))
+
+# Imprimir los enlaces filtrados
+for link in all_productlinks:
+    print(link)
+
+print(len(all_productlinks))
